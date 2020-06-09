2feae6b0429fc25cd2de455ede165104be2662ea
Sumi Sastri <Sumitra.Sastri@gvcgroup.com>
2020-06-09 16:54:44 +0100

    updates store with interfaces & initial state

--- react-redux-ts/src/store.tsx
+++ react-redux-ts/src/store.tsx
@@ -1,12 +1,20 @@
 import React from "react"
 
-const initialState  = {}
-export const Store = React.createContext(initialState)
+interface IState{
+    episodes: [],
+    favourites: []
+}
+
+const initialState: IState  = {
+    episodes:[],
+    favourites: []
+}
+export const Store = React.createContext<IState>(initialState)
 
 
 
 function reducer(){}
 
 export function StoreProvider(props: any): JSX.Element{
-    return <Store.Provider value='test'>{props.children}</Store.Provider>
+    return <Store.Provider value={initialState}>{props.children}</Store.Provider>
 }
