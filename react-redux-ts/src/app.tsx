507891583c7b5a778ba5acd57a3daf5ef3b2b78b
Sumi Sastri <Sumitra.Sastri@gvcgroup.com>
2020-06-09 18:58:30 +0100

    installs @babel/plugin-transform-runtime

--- react-redux-ts/src/app.tsx
+++ react-redux-ts/src/app.tsx
@@ -1,9 +1,25 @@
-import React, { Fragment, useContext } from "react";
-import {Store} from './store'
+import React, { Fragment, useEffect, useContext } from "react";
+import { Store } from "./store";
 
 export default function App(): JSX.Element {
-    const {state,dispatch} = useContext(Store)
-    console.log(state)
+  const { state, dispatch } = useContext(Store);
+  console.log(state);
+
+  useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction();
+  });
+
+  const fetchDataAction = async () => {
+    const URL =
+      "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty-&embed=episodes"
+    const data = await fetch(URL)
+    const dataJSON = await data.json();
+    return dispatch({
+      type: "FETCH_DATA",
+      payload: dataJSON._embedded.episodes
+    });
+  };
+  console.log(state);
   return (
     <div>
       <Fragment>
