483e1773380b9060b8d103cbc9719d8611a590b8
Sumi Sastri <Sumitra.Sastri@gvcgroup.com>
2020-06-09 10:19:18 +0100

    sets up boilder plate for index.tsx as root file

--- react-redux-ts/src/index.tsx
+++ react-redux-ts/src/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
-export default function index() {
+export default function App(): JSX.Element {
     return (
         <div>
-            
+            <h1>Hello World</h1>
         </div>
     )
 }
+
+const root = document.getElementById('app-root')
+
+ReactDOM.render(<App />, root)
