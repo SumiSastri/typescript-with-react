c0e34898cf745df72bab2b54041a33a6502797e8
Sumi Sastri <Sumitra.Sastri@gvcgroup.com>
2020-06-09 10:40:25 +0100

    sets up basic webpack module exports

--- react-redux-ts/webpack.config.js
+++ react-redux-ts/webpack.config.js
@@ -1,1 +1,15 @@
-module.exports = {}
+module.exports = {
+    target: 'web',
+    mode: 'development',
+    entry: './src/index.tsx',
+    output:{
+        path: null,
+        filename: 'bundle.js'
+    },
+    module: null,
+    resolve: {extensions: ['.ts', '.tsx', '.js']},
+    devServer: {
+        contentBase: './',
+        port: 5000
+    }
+}   
